b75696138c7c6e24c2c55dfd587b9452149f56c7
JamzOhJamz <87213748+JamzOhJamz@users.noreply.github.com>
2023-07-15 14:50:14 -0600

    Revert "Merge branch 'development' into reforged"
    This reverts commit 146f307e0573957810c662b9d90ab999465444c8, reversing
    changes made to ba27d06c77633561d5e1b964471b31976fad81f3.

--- api/routes/shop.js
+++ api/routes/shop.js
@@ -4,9 +4,9 @@ export const prefix = '/api/shop';
 
 async function routes(app) {
     // TODO: Implement this route in the project. It currently mirrors the response from official server.
-    app.get('/available', async (req) => {
+    app.get('/available', async () => {
         try {
-            const response = await fetch('http://zombsroyale.io/api/shop/available?' + new URLSearchParams(req.query).toString());
+            const response = await fetch('http://zombsroyale.io/api/shop/available');
             const data = await response.json();
             for (let i = 0; i < data.items.length; i++) {
                 data.items[i].is_stock = true;
@@ -14,7 +14,6 @@ async function routes(app) {
             for (let i = 0; i < data.packs.length; i++) {
                 data.packs[i].can_purchase = true;
             }
-            delete data.itemsSignature;
             return data;
         } catch (error) {
             return {
